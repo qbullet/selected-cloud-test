7a707f354df640a3b8e79e707eb929472690f6f0
NyQuil <47892094+qbullet@users.noreply.github.com>
2021-04-07 16:43:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def Home():
-  return render_template('home.html') 
+  return "my name is PongZa786"
 
-@app.route('/about-us')
-def AboutUs():
-  return render_template('about-us.html')  
-
-@app.route('/product')
-def GetProduct():
-  return jsonify(productName = 'test product')
 
 if __name__ == '__main__':
   app.debug = True
-  app.run(host='0.0.0.0', port=3030)
+  app.run(host='0.0.0.0', port=80)
